6c17fb5e0742060dd74f5f41be1cf943dedec4ed
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-03-20 09:52:39 +0100

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -12,28 +12,29 @@ export function activate(context: vscode.ExtensionContext) {
         .split("/")
         .slice(0, -1)
         .join("/");
-
-      console.log("folder: ", folder);
-      if (folder) {
-        fs.promises.readdir(folder).then((files) => {
-          const filtered = files.filter(junk.not);
-          filtered.forEach((file) => {
-            const uri = Uri.joinPath(Uri.file(folder!), file);
-            if (!fs.lstatSync(uri.fsPath).isDirectory()) {
-              list.push(file);
-            }
-          });
-          vscode.window.showQuickPick(list, {}).then((selection) => {
-            list = [];
-            if (!selection || !folder) {
-              return;
-            }
-
-            const uri = Uri.joinPath(Uri.file(folder), selection);
-            vscode.window.showTextDocument(uri);
-          });
-        });
+      if (!folder) {
+        vscode.window.showWarningMessage("Can't find parent folder.");
+        return;
       }
+
+      fs.promises.readdir(folder).then((files) => {
+        const filtered = files.filter(junk.not);
+        filtered.forEach((file) => {
+          const uri = Uri.joinPath(Uri.file(folder!), file);
+          if (!fs.lstatSync(uri.fsPath).isDirectory()) {
+            list.push(file);
+          }
+        });
+        vscode.window.showQuickPick(list, {}).then((selection) => {
+          list = [];
+          if (!selection || !folder) {
+            return;
+          }
+
+          const uri = Uri.joinPath(Uri.file(folder), selection);
+          vscode.window.showTextDocument(uri);
+        });
+      });
     }
   );
 
